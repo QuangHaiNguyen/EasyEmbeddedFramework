fe70654251b93fcce0889c66854a1daa89bce162
Hai Nguyen <hainguyen.eeit@gmail.com>
2022-04-07 23:39:25 +0200

    Add - test can be activate via list of macro
    This implemetation makes it easier to turn on and off tests
    
     On branch features/system_error
     Changes to be committed:
    	modified:   tests/unit_tests/vs_unittests/test.cpp

--- tests/unit_tests/vs_unittests/test.cpp
+++ tests/unit_tests/vs_unittests/test.cpp
@@ -2,15 +2,64 @@
 #include <limits.h>
 #include "gtest/gtest.h"
 
-//#include "unit_test_ipc.h"
-//#include "unit_test_binParser.h"
-//#include "unit_test_assert.h"
-//#include "unit_test_hexdump.h"
-//#include "../unit_test_cli.h"
-//#include "../unit_test_linked_list.h"
-//#include "../unit_test_smalloc.h"
-//#include "../unit_test_ring_buffer.h"
-//#include "../unit_test_stcmem.h"
-//#include "../unit_test_kernel.h"
-//#include "../unit_test_printf.h"
+
+#define TEST_IPC                0U
+#define TEST_BIN_PARSER         0U
+#define TEST_ASSERT             0U
+#define TEST_HEXDUMP            0U
+#define TEST_CLI                0U
+#define TEST_LINKEDLIST         0U
+#define TEST_SMALLOC            0U
+#define TEST_RINGBUFFER         0U
+#define TEST_STCMEM             0U
+#define TEST_KERNEL             0U
+#define TEST_PRINTF             0U
+#define TEST_SYSTEM_ERROR       1U
+
+#if (TEST_IPC == 1U)
+#include "../unit_test_ipc.h"
+#endif /* TEST_IPC */
+
+#if (TEST_BIN_PARSER == 1U)
+#include "../unit_test_binParser.h"
+#endif /* TEST_BIN_PARSER */
+
+#if (TEST_ASSERT == 1U)
+#include "../unit_test_assert.h"
+#endif /* TEST_ASSERT */
+
+#if (TEST_HEXDUMP == 1U)
+#include "unit_test_hexdump.h"
+#endif /* TEST_HEXDUMP */
+
+#if (TEST_CLI == 1U)
+#include "../unit_test_cli.h"
+#endif /* TEST_CLI */
+
+#if (TEST_LINKEDLIST == 1U)
+#include "../unit_test_linked_list.h"
+#endif /* TEST_LINKEDLIST */
+
+#if (TEST_SMALLOC == 1U)
+#include "../unit_test_smalloc.h"
+#endif /* TEST_SMALLOC */
+
+#if (TEST_RINGBUFFER == 1U)
+#include "../unit_test_ring_buffer.h"
+#endif /* TEST_RINGBUFFER */
+
+#if (TEST_STCMEM == 1U)
+#include "../unit_test_stcmem.h"
+#endif /* TEST_STCMEM */
+
+#if (TEST_KERNEL == 1U)
+#include "../unit_test_kernel.h"
+#endif /* TEST_KERNEL */
+
+#if (TEST_PRINTF == 1U)
+#include "../unit_test_printf.h"
+#endif /* TEST_PRINTF */
+
+#if (TEST_SYSTEM_ERROR == 1U)
 #include "../unit_test_system_error.h"
+#endif /* TEST_SYSTEM_ERROR */
