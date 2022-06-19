7e29838b3ee145ff6cea0fdf8b93875551d90630
Hai Nguyen <hainguyen.eeit@gmail.com>
2022-06-19 17:52:58 +0200

    IMPLEMENT - add data model module to cmake build

--- ezmsdk/app/app_config.h
+++ ezmsdk/app/app_config.h
@@ -15,7 +15,7 @@
 #define _APP_CONFIG_H
 
 
-#define CLI                     1U
+#define CLI                     0U
 #define SCHEDULER               0U
 #define SMALLOC                 0U
 #define BIN_PARSER              1U
@@ -31,7 +31,7 @@
 #define KERNEL                  1U
 #define DRIVERINF               1U
 #define EZM_PRINTF              1U
-#define HAL_UART                1U
+#define HAL_UART                0U
 #define SYSTEM_ERROR            1U
 #define FLASH_SIM               0U
 #define EVENT_NOTIFIER          1U
