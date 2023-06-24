811501441191b3318e2b3749d6d29556714140e0
Hai Nguyen <hainguyen.eeit@gmail.com>
2023-06-24 15:28:57 +0200

    Implement - expand the assert module
    Expend the module to have more flexibility in assertation

--- ezmsdk/utilities/assert/ez_assert.h
+++ ezmsdk/utilities/assert/ez_assert.h
@@ -32,11 +32,27 @@
 
 #if (EZ_ASSERT == 1U)
 #include "logging/ez_logging.h"
-#define ASSERT(expr)            if(!(expr)) {dbg_print("assert:%s:%d\n", __func__ , __LINE__);}
-#define ASSERT_MSG(expr, msg)   if(!(expr)) {dbg_print("assert:%s:%d - %s\n", __func__ , __LINE__, msg);}
+#define ASSERT(expr) \
+            if(!(expr)) {dbg_print("\nassert:%s:%d\n", __func__ , __LINE__);while(1);}
+
+#define ASSERT_MSG(expr, msg)                                                           \
+            do                                                                          \
+            {                                                                           \
+                if(!(expr))                                                             \
+                {dbg_print("\nassert:%s:%d - %s\n", __func__ , __LINE__, msg);while(1);} \
+            }while(0);                                                                  \
+
+#define ASSERT_CUST_MSG(expr, fmt, ...) \
+            do                                                                                      \
+            {                                                                                       \
+                if(!(expr))                                                                         \
+                {printf("\nassert:%s:%d - " fmt "\n", __func__ , __LINE__, ##__VA_ARGS__);while(1);}\
+            }while(0);                                                                              \
+            
 #else
 #define ASSERT(expr)
 #define ASSERT_MSG(expr, msg)
+#define ASSERT_CUST_MSG(expr, msg, ...)
 #endif /* HELPER_ASSERT */
 
 /**< register your module id*/
